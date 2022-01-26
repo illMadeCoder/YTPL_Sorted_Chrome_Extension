1b3d083f6b993dc3b190a108f7e4a1d19bec1531
illMadeCoder <illMadeCoder@gmail.com>
2022-01-26 12:05:47 -0500

    hits ytplsorted api locally and produces a sorted playlist from whatever page you're on

--- popup.js
+++ popup.js
@@ -6,11 +6,24 @@ chrome.storage.sync.get("color", ({ color }) => {
 
 changeColor.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({active: true, currentWindow: true})
-
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: setPageBackgroundColor,
-    })
+    //chrome.tabs.create({ url: tab.url });
+    // chrome.scripting.executeScript({
+    //     target: { tabId: tab.id },
+    //     function: setPageBackgroundColor,
+    // })
+    //PLA26855F88564F001
+    let playlistIDre = /list=(.*)/
+    let playlistID = playlistIDre.exec(tab.url)[1]
+    fetch(`http://localhost:8000/ytplsorted?playlistIDs=${playlistID}&sortby=views`)
+        .then(r => r.text())
+        .then(url => chrome.tabs.create({ url: url }))
+        .catch(e => console.log(e))
+    
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+        //let url = tabs[0].url;
+                
+        // use `url` here inside the callback because it's asynchronous!
+        });
 })
 
 function setPageBackgroundColor() {
