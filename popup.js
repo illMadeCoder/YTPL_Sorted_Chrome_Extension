4dcb524b53abf033e089ae47ee507b92c6b966c2
illMadeCoder <illMadeCoder@gmail.com>
2022-01-26 12:58:29 -0500

    use extension buttons to add multiple playlists, does not yet work across tabs

--- popup.js
+++ popup.js
@@ -1,10 +1,11 @@
-let changeColor = document.getElementById("changeColor");
+let addToPlaylistList = document.getElementById("addToPlaylistListButton");
+let submitButton = document.getElementById("submitButton");
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
-
-changeColor.addEventListener("click", async () => {
+// chrome.storage.sync.get("color", ({ color }) => {
+//   changeColor.style.backgroundColor = color;
+// });
+chrome.storage.sync.set({playlistIDs: []})
+addToPlaylistList.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({active: true, currentWindow: true})
     //chrome.tabs.create({ url: tab.url });
     // chrome.scripting.executeScript({
@@ -14,20 +15,26 @@ changeColor.addEventListener("click", async () => {
     //PLA26855F88564F001
     let playlistIDre = /list=(.*)/
     let playlistID = playlistIDre.exec(tab.url)[1]
-    fetch(`http://localhost:8000/ytplsorted?playlistIDs=${playlistID}&sortby=views`)
-        .then(r => r.text())
-        .then(url => chrome.tabs.create({ url: url }))
-        .catch(e => console.log(e))
-    
-    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-        //let url = tabs[0].url;
-                
-        // use `url` here inside the callback because it's asynchronous!
-        });
+    chrome.storage.sync.get('playlistIDs', (data) => {   
+        playlistIDs = data.playlistIDs     
+        console.log(playlistIDs)
+        console.log(playlistID)
+        playlistIDs.push(playlistID)
+        chrome.storage.sync.set({playlistIDs: playlistIDs})
+    })
+    // fetch(`http://localhost:8000/ytplsorted?playlistIDs=${playlistID}&sortby=views`)
+    //     .then(r => r.text())
+    //     .then(url => cosneol.log(url))
+    //     .catch(e => console.log(e))    
 })
 
-function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-        document.body.style.backgroundColor = color;
+submitButton.addEventListener("click", async () => {
+    chrome.storage.sync.get('playlistIDs', (data) => {       
+        playlistIDsCSV = data.playlistIDs.join(",")
+        fetch(`http://localhost:8000/ytplsorted?playlistIDs=${playlistIDsCSV}&sortby=views`)
+            .then(r => r.text())
+            .then(url => chrome.tabs.create({ url: url }))
+            .catch(e => console.log(e))     
+        
     })
-}
+})
