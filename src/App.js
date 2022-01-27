2a6bce0b3f3dba3b82d3711989c76549b847e7a8
illMadeCoder <illMadeCoder@gmail.com>
2022-01-27 16:20:50 -0500

    playlistIDs display

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React, { useState } from 'react';
 
 function App() {
   const [playlistIDs, setPlaylistIDs] = useState([])
-  chrome.storage.sync.get("playlistIDs", data=>setPlaylistIDs(data.playlistIDs))
+  chrome.storage.sync.get("playlistIDs", ({playlistIDs})=>setPlaylistIDs(playlistIDs))
   return (
     <div className="App">
       <header className="App-header">
@@ -25,20 +25,24 @@ async function cancel() {
   chrome.storage.sync.set({playlistIDs: []})
 }
 
-async function appendPlaylist() {
+async function appendPlaylist(playlistIDs) {
   let [tab] = await chrome.tabs.query({active: true, currentWindow: true})
   let playlistIDre = /list=([^&]*)/
   let playlistID = playlistIDre.exec(tab.url)[1]
-  chrome.storage.sync.get({'playlistIDs': []}, ({playlistIDs}) => {         
-      playlistIDs.push(playlistID)
-      chrome.storage.sync.set({playlistIDs: playlistIDs})
+  chrome.storage.sync.get({'playlistIDs': []}, ({playlistIDs}) => {
+      if (!playlistIDs.includes(playlistID)) {
+        playlistIDs.push(playlistID)
+        chrome.storage.sync.set({playlistIDs: playlistIDs})
+      } else {
+        // TODO error playlist already in list
+      }
   })    
 }
 
 async function submit() {
   chrome.storage.sync.get({'playlistIDs': []}, (data) => {       
     let playlistIDsCSV = data.playlistIDs.join(",")
-    fetch(`http://localhost:8000/ytplsorted?playlistIDs=${playlistIDsCSV}&sortby=views`)
+    fetch(`http://localhost:8000/ytplsorted?playlistIDs=${playlistIDsCSV}&sortby=ratio`)
         .then(r => r.text())
         .then(url => chrome.tabs.create({ url: url }))
         .catch(e => console.log(e))        
